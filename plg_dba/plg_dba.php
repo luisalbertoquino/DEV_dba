dc366ea016aa5f0443d3bd4c9a7ec3cc460ca238
luisalbertoquino <50123911+luisalbertoquino@users.noreply.github.com>
2023-06-08 11:17:20 -0500

    Archivos iniciales con las librerias basicas

---
+++ plg_dba/plg_dba.php
@@ -0,0 +1,15 @@
+<?php
+/** 
+* Plugin Name:  plg_dba
+* Plugin URI: http://wpplugin.es
+* Description: Plugin para representración de datos por medio de formularios de registro de información
+* Version: 1.0.0
+* Author: Luis Quino
+* Author URI: https://github.com/luisalbertoquino/UninavarraDev
+* Requires at least: 4.0
+* Tested up to: 4.3
+*
+*Text Domain: wpplugin-ejemplo
+* Domain path: /languages/
+*/
+defined( 'ABSPATH' ) or die( '¡sin trampas!’ );
